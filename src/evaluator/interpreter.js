9793ec92d62e6ab54b455923362041faed300d1d
Alfio Martini <alfio.martini@gmail.com>
2021-11-13 20:38:04 -0300

    Added an environment to the eval function (problem: bindings are global this way, as opposed to the previous version with closures)

--- src/evaluator/interpreter.js
+++ src/evaluator/interpreter.js
@@ -1,79 +1,89 @@
-// global definitions
-// const symbolTable = new Map();
 
+const topEnv = Object.create(null);
+// initialize topEnv
 
-const start = () => {
-  // now symbol table is local to each program evaluation
-  const symbolTable = new Map();
-  return function eval(exprTree){
-    switch (exprTree.type) {
-      case "string":
-        return exprTree.value;
-      case "number":
-        return parseFloat(exprTree.value);
-        break;
-      case "identifier":
-        const id = exprTree.name;
-        if (symbolTable.has(id)){
-          return symbolTable.get(id)
+for (let op of ['+','-','*','/', '<','>', '<=','>=']){
+  topEnv[op] =  new Function("x, y", `return x ${op} y;`);
+}
+
+// env is a map
+function eval(exprTree, env){
+  switch (exprTree.type) {
+    case "string":
+      return exprTree.value;
+    case "number":
+      return parseFloat(exprTree.value);
+      break;
+    case "identifier":
+      const id = exprTree.name;
+      if (id in env){
+        return env[id]
+      } else {
+        throw new SyntaxError(`identifier ${id} is not defined.`)
+      };
+    case "apply":
+      let {operator, args} = exprTree;
+      if (operator.name === "+"){
+        return  eval(args[0], env) + eval(args[1], env);
+      };
+      if (operator.name === "*"){
+        return  eval(args[0], env) * eval(args[1], env);
+      };
+      if (operator.name === "-"){
+        return  eval(args[0], env) -  eval(args[1], env);
+      };
+      if (operator.name === "/"){
+        return  eval(args[0], env) / eval(args[1], env);
+      };
+      if (operator.name === '>'){
+        return eval(args[0], env) > eval(args[1], env);
+      };
+      if (operator.name === '<'){
+        return eval(args[0], env) < eval(args[1], env);
+      };
+      if (operator.name === 'define'){
+        env[args[0].name] = eval(args[1], env);
+      };
+      if (operator.name === 'do'){
+        let val;
+        for (let arg of args){
+          val = eval(arg, env);
+        }
+        // return val;
+      };
+      if (operator.name === 'print'){
+        let val = eval(args[0], env);
+        console.log(val);
+        // return val;
+      }
+      if (operator.name === 'if'){
+        if (eval(args[0], env)){
+          eval(args[1], env);
         } else {
-          throw new SyntaxError(`identifier ${id} is not defined.`)
-        };
-      case "apply":
-        let {operator, args} = exprTree;
-        if (operator.name === "+"){
-          return  eval(args[0]) + eval(args[1]);
-        };
-        if (operator.name === "*"){
-          return  eval(args[0]) * eval(args[1]);
-        };
-        if (operator.name === "-"){
-          return  eval(args[0]) -  eval(args[1]);
-        };
-        if (operator.name === "/"){
-          return  eval(args[0]) / eval(args[1]);
-        };
-        if (operator.name === '>'){
-          return eval(args[0]) > eval(args[1]);
-        };
-        if (operator.name === '<'){
-          return eval(args[0]) < eval(args[1]);
-        };
-        if (operator.name === 'define'){
-          symbolTable.set(args[0].name, eval(args[1]))
-        };
-        if (operator.name === 'do'){
-          for (let arg of args){
-            eval(arg);
-          }
-        };
-        if (operator.name === 'print'){
-          console.log(eval(args[0]));
+          eval(args[2], env);
         }
-        if (operator.name === 'if'){
-          if (eval(args[0])){
-            eval(args[1]);
-          } else {
-            eval(args[2]);
-          }
-        };
-        if (operator.name === 'while'){
-          while (eval(args[0])){
-            eval(args[1]);
-          }
+      };
+      if (operator.name === 'while'){
+        while (eval(args[0], env)){
+          eval(args[1], env);
         }
-        break;
-    }
+      }
+      break;
   }
 }
 
 
 function evalPrg(text){
-  const eval = start();
   try {
-    eval(parsePrg(text).expr);
+    eval(parsePrg(text).expr, topEnv);
   } 
   catch (error){
    console.log(error.message);
+  }
+}
+
+const runAll = () => {
+  for (let prg of programs){
+    evalPrg(prg);
   }
 }
--- src/examples/examples.js
+++ src/examples/examples.js
@@ -7,10 +7,10 @@ const prg02 = `
     do(define(a,5),
        define(x, +(a,10)),
        print(x))`;
-const prg03 = `define(house, "large")`;
-const prg04 = `+(5,10)`; // 15
-const prg05 = `*(3, +(5,10))`; // 45
-const prg06 = `"Hello World!"`;
+const prg03 = `do(define(house, "red house"), print(house))`;
+const prg04 = `do(define(x,*(5,10)), print(x))`; // 50
+const prg05 = `do(define(x,*(3, +(5,10))), print(x))`; // 45
+const prg06 = `print(x)`;
 const prg07 = `256`;
 const prg08 = `-(+(20,30), *(15,10))`; // -100
 const prg09 = `define(x, 10)`;
@@ -24,3 +24,17 @@ const prg11 = `
          define(count, +(count, 1)))),
       print(total))
 `;
+
+const programs = [
+  prg01,
+  prg02,
+  prg03,
+  prg04,
+  prg05,
+  prg06,
+  prg07,
+  prg08,
+  prg09,
+  prg10,
+  prg11,
+];
