a6ac483b3f990ccc5fb43c7b9e18c0dc9d3c8209
Alfio Martini <alfio.martini@gmail.com>
2021-11-13 06:22:07 -0300

    Defined Symbol Table to be local to each program (closures)

--- src/evaluator/interpreter.js
+++ src/evaluator/interpreter.js
@@ -1,57 +1,74 @@
-const symbolTable = new Map();
+// global definitions
+// const symbolTable = new Map();
 
-function eval(exprTree){
-  switch (exprTree.type) {
-    case "string":
-      return exprTree.value;
-    case "number":
-      return parseFloat(exprTree.value);
-      break;
-    case "identifier":
-      const id = exprTree.value;
-      if (symbolTable.has(id)){
-        return symbolTable.get(id)
-      } else {
-        throw new SyntaxError(`identifier ${id} is not defined.`)
-      };
-    case "apply":
-      let {expr, args} = exprTree;
-      if (expr.value === "+"){
-        return  eval(args[0]) + eval(args[1]);
-      };
-      if (expr.value === "*"){
-        return  eval(args[0]) * eval(args[1]);
-      };
-      if (expr.value === "-"){
-        return  eval(args[0]) -  eval(args[1]);
-      };
-      if (expr.value === "/"){
-        return  eval(args[0]) / eval(args[1]);
-      };
-      if (expr.value === '>'){
-        return eval(args[0]) > eval(args[1]);
-      };
-      if (expr.value === '<'){
-        return eval(args[0]) < eval(args[1]);
-      };
-      if (expr.value === 'define'){
-        symbolTable.set(args[0].value, eval(args[1]))
-      };
-      if (expr.value === 'do'){
-        for (let arg of args){
-          eval(arg);
-        }
-      };
-      if (expr.value === 'print'){
-        console.log(eval(args[0]));
-      }
-      if (expr.value === 'if'){
-        if (eval(args[0])){
-          eval(args[1]);
+
+const start = () => {
+  // now symbol table is local to each program evaluation
+  const symbolTable = new Map();
+  return function eval(exprTree){
+    switch (exprTree.type) {
+      case "string":
+        return exprTree.value;
+      case "number":
+        return parseFloat(exprTree.value);
+        break;
+      case "identifier":
+        const id = exprTree.value;
+        if (symbolTable.has(id)){
+          return symbolTable.get(id)
         } else {
-          eval(args[2]);
+          throw new SyntaxError(`identifier ${id} is not defined.`)
+        };
+      case "apply":
+        let {expr, args} = exprTree;
+        if (expr.value === "+"){
+          return  eval(args[0]) + eval(args[1]);
+        };
+        if (expr.value === "*"){
+          return  eval(args[0]) * eval(args[1]);
+        };
+        if (expr.value === "-"){
+          return  eval(args[0]) -  eval(args[1]);
+        };
+        if (expr.value === "/"){
+          return  eval(args[0]) / eval(args[1]);
+        };
+        if (expr.value === '>'){
+          return eval(args[0]) > eval(args[1]);
+        };
+        if (expr.value === '<'){
+          return eval(args[0]) < eval(args[1]);
+        };
+        if (expr.value === 'define'){
+          symbolTable.set(args[0].value, eval(args[1]))
+        };
+        if (expr.value === 'do'){
+          for (let arg of args){
+            eval(arg);
+          }
+        };
+        if (expr.value === 'print'){
+          console.log(eval(args[0]));
         }
-      }
-      break;
+        if (expr.value === 'if'){
+          if (eval(args[0])){
+            eval(args[1]);
+          } else {
+            eval(args[2]);
+          }
+        }
+        break;
+    }
+  }
+}
+
+
+function evalPrg(text){
+  const eval = start();
+  try {
+    eval(parsePrg(text).expr);
+  } 
+  catch (error){
+   console.log(error.message);
   }
 }
