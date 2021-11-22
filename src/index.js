a2b38cc2d8c879de2f2cf130c6c60b181b7dac0e
Alfio Martini <alfio.martini@gmail.com>
2021-11-22 11:44:12 -0300

    Implemented first version of UI for the language evaluator (fixed)

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import { programs } from "./examples/examples.js";
+import { evalPrg } from "./evaluator/interpreter.js";
+// https://stackoverflow.com/questions/10911526/how-do-i-change-an-html-selected-option-using-javascript
+
+const textarea = document.getElementById("prgtext");
+const evalBtn = document.getElementById("eval");
+const result = document.getElementById("result");
+const select = document.getElementById("programs");
+buildSelect();
+
+select.addEventListener("change", evalSelect);
+evalBtn.addEventListener("click", evalTextArea);
+
+function buildSelect() {
+  programs.forEach((prg, index) => {
+    const option = document.createElement("option");
+    option.value = index + 1;
+    option.innerText = `prg${index}`;
+    select.append(option);
+  });
+  let selected = 1;
+  select.value = selected;
+  textarea.value = programs[selected - 1];
+}
+function evalSelect(event) {
+  textarea.value = programs[event.target.value - 1];
+}
+function evalTextArea() {
+  const prg = textarea.value;
+  const val = evalPrg(prg);
+  result.innerText = "> " + val;
+}
